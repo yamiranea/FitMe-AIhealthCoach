56b38e7b3e5954ca5d87c2734280f6ba8c2322a0
yamiranea <yamilarenea@gmail.com>
2024-03-13 12:30:59 +0100

    Create factories

--- backend/database/factories/SportPlanFactory.php
+++ backend/database/factories/SportPlanFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SportPlan>
  */
-class UserFactory extends Factory
+class SportPlanFactory extends Factory
 {
     /**
      * Define the model's default state.
