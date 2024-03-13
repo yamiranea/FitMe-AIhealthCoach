7d40ac0e0297914d0aec005535349175c99c0089
yamiranea <yamilarenea@gmail.com>
2024-03-13 12:28:17 +0100

    Define models for NutritionalPlan, SportPlan and User

--- backend/app/Models/NutritionalPlan.php
+++ backend/app/Models/NutritionalPlan.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class NutritionalPlan extends Model
 {
     use HasFactory;
+
+    protected $table = 'nutritional_plans';
+
+    protected $fillable = [
+        'description',
+        'diet_type',
+        'current_weight',
+        'creation_user',
+        'updated_user',
+        'creation_date',
+        'updated_date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'age',
+        'gender',
+        'current_weight',
+        'height',
+        'updated_date',
+        'id_nutritional_plan',
+        'id_sport_plan',
+    ];
+
+    public function nutritionalPlan()
+    {
+        return $this->hasOne(NutritionalPlan::class, 'ID_plan', 'id_nutritional_plan');
+    }
+
+    public function sportPlan()
+    {
+        return $this->hasOne(SportPlan::class, 'id_plan', 'id_sport_plan');
+    }
 }
