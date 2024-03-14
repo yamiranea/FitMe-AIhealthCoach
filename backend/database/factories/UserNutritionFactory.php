f34f333dd593d514461fba055909adc22149f5c1
yamiranea <yamilarenea@gmail.com>
2024-03-14 12:52:32 +0100

    Create factories

--- backend/database/factories/UserNutritionFactory.php
+++ backend/database/factories/UserNutritionFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserNutrition>
  */
-class UserFactory extends Factory
+class UserNutritionFactory extends Factory
 {
     /**
      * Define the model's default state.
