03fd8d1b601c5cc385456f583ae79d31f3520a81
yamiranea <yamilarenea@gmail.com>
2024-03-14 13:45:02 +0100

    Create and delete factories

--- backend/database/factories/UserNutritionFactory.php
+++ backend/database/factories/UserNutritionFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\NutritionalPlan;
+use App\Models\NutritionalTag;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +19,12 @@ class UserNutritionFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            //
-        ];
+         return [
+        'id_user' => User::factory(),
+        'id_nutritional_plan' => NutritionalPlan::all()->random()->id,
+        'id_nutritional_tag' => NutritionalTag::all()->random()->id,
+        'created_date' => $this->faker->date(),
+        'updated_date' => $this->faker->date(),        
+    ];
     }
 }
